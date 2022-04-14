401527b08722296996a8bd45fe95fe6e3032e6e3
Waqar Ali Siyal <sirai.waqar.ali@gmail.com>
2022-04-14 19:50:24 +0500

    First Commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();                       // Create expressjs object
 
 // Connect MongoDb
 // mongoose.connect('mongodb://localhost:27017/BlogDb');    // Local
-mongoose.connect('mongodb://BlogUser:bloguser123@cluster0-shard-00-00.mourb.mongodb.net:27017,cluster0-shard-00-01.mourb.mongodb.net:27017,cluster0-shard-00-02.mourb.mongodb.net:27017/BlogDb?ssl=true&replicaSet=atlas-jdfk2u-shard-0&authSource=admin&retryWrites=true&w=majority');
+mongoose.connect('mongodb://user:password@cluster0-shard-00-00.mourb.mongodb.net:27017,cluster0-shard-00-01.mourb.mongodb.net:27017,cluster0-shard-00-02.mourb.mongodb.net:27017/BlogDb?ssl=true&replicaSet=atlas-jdfk2u-shard-0&authSource=admin&retryWrites=true&w=majority');
 // Check connection is established
 const connection = mongoose.connection;
 connection.once('open', () => {
